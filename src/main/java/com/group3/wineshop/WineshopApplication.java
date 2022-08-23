41e09269272cf4f6c61c1721f81e051c69e5ec8a
Jordi Gómez <jigg@gft.com>
2022-08-23 10:38:38 +0200

    initial project setup

---
+++ src/main/java/com/group3/wineshop/WineshopApplication.java
@@ -0,0 +1,13 @@
+package com.group3.wineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WineshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group3/wineshop/WineshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group3.wineshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
