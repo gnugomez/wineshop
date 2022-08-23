65b3a644b1253172d18f7e73ddd3b73e060e735c
Jordi Gómez <jigg@gft.com>
2022-08-23 13:22:55 +0200

    chore(ci): exclude datasource

--- src/main/java/com/group3/wineshop/WineshopApplication.java
+++ src/main/java/com/group3/wineshop/WineshopApplication.java
@@ -2,8 +2,9 @@ package com.group3.wineshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class WineshopApplication {
 
     public static void main(String[] args) {
